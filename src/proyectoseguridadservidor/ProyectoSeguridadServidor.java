609b690fca1e50dcf4c2433afeced99a988d72a5
carol <carol@CarolinaPC>
2016-05-12 00:09:06 -0500

    cambio en peticion de usuario

--- src/proyectoseguridadservidor/ProyectoSeguridadServidor.java
+++ src/proyectoseguridadservidor/ProyectoSeguridadServidor.java
@@ -5,7 +5,6 @@
  */
 package proyectoseguridadservidor;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -241,7 +240,7 @@ public class ProyectoSeguridadServidor {
                 String claveServidor = llaveServidor(numeroA, usuarios.get(name).getPassword(), numeroU, b);
                 System.out.println("clave Servidor: " + claveServidor);
                 SecretKey llave_compartida = GenerarPass(claveServidor);
-                
+
                 while (true) {
 
                     String ingreso = in.readLine();
@@ -249,7 +248,7 @@ public class ProyectoSeguridadServidor {
                         return;
                     } else {
                         ingreso = new String(DecryptAES(ingreso, llave_compartida));
-                        
+
                         String[] texto = ingreso.split(" ");
                         String autenticado = texto[0];
 
@@ -275,47 +274,51 @@ public class ProyectoSeguridadServidor {
                 while (true) {
 
                     String ingreso = in.readLine();
+
                     if (ingreso == null) {
                         return;
-                    } else if (ingreso.startsWith("TALKTO")) {
-                        String[] texto = ingreso.split(" ");
-                        usuarioSeleccionado = texto[2];
-                        if (usuarios.containsKey(usuarioSeleccionado)) {
-                            System.out.println("Usuario Correcto");
-                            for (HashMap.Entry<String, PrintWriter> entry : writers.entrySet()) {
-                                String key = entry.getKey();
-                                if (key.equals(usuarioSeleccionado)) {
-                                    PrintWriter w = entry.getValue();
-                                    w.println("CONEXION " + name);
-                                    System.out.println("Enviando conexion");
-                                    break;
+                    } else {
+                        ingreso = new String(DecryptAES(ingreso, llave_compartida));
+                        if (ingreso.startsWith("TALKTO")) {
+                            String[] texto = ingreso.split(" ");
+                            usuarioSeleccionado = texto[2];
+                            if (usuarios.containsKey(usuarioSeleccionado)) {
+                                System.out.println("Usuario Correcto");
+                                for (HashMap.Entry<String, PrintWriter> entry : writers.entrySet()) {
+                                    String key = entry.getKey();
+                                    if (key.equals(usuarioSeleccionado)) {
+                                        PrintWriter w = entry.getValue();
+                                        w.println("CONEXION " + name);
+                                        System.out.println("Enviando conexion");
+                                        break;
 
+                                    }
+                                }
+                            } else {
+                                out.println("REJECT");
+                                socket.close();
+                                break;
+                            }
+
+                        } else if (ingreso.startsWith("LISTA")) {
+
+                            String userA = "";
+                            for (Usuario value : usuarios.values()) {
+                                if (value.getActivo().equals("Activo")) {
+                                    userA += " " + value.getNombreUsuario();
                                 }
                             }
+                            out.println("USUARIOSACTIVOS" + " " + userA);
+
+                        } else if (ingreso.equals("ACEPTO")) {
+                            System.out.println("USUARIO ACEPTADO");
+
+                            // SE DEBE AGREGAR LA FUNCION DE CREAR LAS DOS CLAVES ENTRE LOS DOS CLIENTES Y MANDAR A CADA UNO. 
                         } else {
                             out.println("REJECT");
                             socket.close();
                             break;
                         }
-
-                    } else if (ingreso.startsWith("LISTA")) {
-
-                        String userA = "";
-                        for (Usuario value : usuarios.values()) {
-                            if (value.getActivo().equals("Activo")) {
-                                userA += " " + value.getNombreUsuario();
-                            }
-                        }
-                        out.println("USUARIOSACTIVOS" + " " + userA);
-
-                    } else if (ingreso.equals("ACEPTO")) {
-                        System.out.println("USUARIO ACEPTADO");
-
-                        // SE DEBE AGREGAR LA FUNCION DE CREAR LAS DOS CLAVES ENTRE LOS DOS CLIENTES Y MANDAR A CADA UNO. 
-                    } else {
-                        out.println("REJECT");
-                        socket.close();
-                        break;
                     }
 
                 }
@@ -437,30 +440,30 @@ public class ProyectoSeguridadServidor {
             return llaveServidor;
         }
 
-    public String EncriptarAES(byte[] texto,SecretKey key ) throws NoSuchAlgorithmException, NoSuchPaddingException, javax.management.openmbean.InvalidKeyException, IllegalBlockSizeException, BadPaddingException, java.security.InvalidKeyException{
+        public String EncriptarAES(byte[] texto, SecretKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, javax.management.openmbean.InvalidKeyException, IllegalBlockSizeException, BadPaddingException, java.security.InvalidKeyException {
 
-        Cipher AesCipher = Cipher.getInstance("AES");
-        AesCipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] byteCipherText = AesCipher.doFinal(texto);
-        String Texto = new BASE64Encoder().encode(byteCipherText);
-        return Texto;
-    }
-    
-    public byte[] DecryptAES(String cipherText,SecretKey key ) throws NoSuchAlgorithmException, javax.management.openmbean.InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, java.security.InvalidKeyException, IOException{
-        byte[] Texto = new BASE64Decoder().decodeBuffer(cipherText);
-        Cipher AesCipher = Cipher.getInstance("AES");
-        AesCipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] bytePlainText = AesCipher.doFinal(Texto);
-        return bytePlainText;
-    }
-    
-    public SecretKey GenerarPass(String Hash) throws NoSuchAlgorithmException{
-       byte[] key = Hash.getBytes();
-       MessageDigest sha = MessageDigest.getInstance("SHA-1");
-       key = sha.digest(key);
-       key = Arrays.copyOf(key, 16); // use only first 128 bit
-       SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-       return secretKeySpec;
-    }
+            Cipher AesCipher = Cipher.getInstance("AES");
+            AesCipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] byteCipherText = AesCipher.doFinal(texto);
+            String Texto = new BASE64Encoder().encode(byteCipherText);
+            return Texto;
+        }
+
+        public byte[] DecryptAES(String cipherText, SecretKey key) throws NoSuchAlgorithmException, javax.management.openmbean.InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, java.security.InvalidKeyException, IOException {
+            byte[] Texto = new BASE64Decoder().decodeBuffer(cipherText);
+            Cipher AesCipher = Cipher.getInstance("AES");
+            AesCipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] bytePlainText = AesCipher.doFinal(Texto);
+            return bytePlainText;
+        }
+
+        public SecretKey GenerarPass(String Hash) throws NoSuchAlgorithmException {
+            byte[] key = Hash.getBytes();
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16); // use only first 128 bit
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+            return secretKeySpec;
+        }
     }
 }
